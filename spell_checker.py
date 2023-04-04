5dd4518df4aa9e8a06a512c96d26eeb5c5318080
kate-holdener <42073871+kate-holdener@users.noreply.github.com>
2023-04-04 21:13:01 +0000

    added replace, delete, add transformations for python

--- spell_checker.py
+++ spell_checker.py
@@ -7,3 +7,9 @@ word_list = sentence.split(' ')
 for word in word_list:
     if not english_words.is_word(word.strip()):
         print('correcting', word)
+        corrections = english_words.get_replace_corrections(word)
+        print('Replace suggestions: ', corrections)
+        corrections = english_words.get_delete_corrections(word)
+        print('Delete suggestions: ', corrections)
+        corrections = english_words.get_add_corrections(word)
+        print('Add suggestions: ', corrections)
--- words.py
+++ words.py
@@ -10,4 +10,46 @@ class Words:
 
     def get_size(self):
         return len(self.words)
+
+    def get_replace_corrections(self, misspelled_word):
+        suggestions = []
+        for w in self.words:
+            # only consider words that have the same length as misspelled_word
+            if len(w) == len(misspelled_word):
+                # try replacing one letter at a time and see if we can transform the misspelled word into word w
+                for index in range(0, len(w)):
+                    correction = misspelled_word[0:index]+w[index]
+                    if index < len(w)-1:
+                        correction+=misspelled_word[index+1:]
+                    # test if replaced letter transforms our word to w    
+                    if correction == w:
+                        suggestions.append(w)
+                        break
+        return suggestions
+        
+    def get_delete_corrections(self, misspelled_word):
+        suggestions = []
+        for w in self.words:
+            # only consider words that are one character shorter than misspelled_word
+            if len(w) == len(misspelled_word)-1:
+                # try deleting one character from misspelled word at a time and see if it transforms to word w
+                for index in range(0, len(w)):
+                    correction = misspelled_word[0:index]
+                    if index < len(w)-1:
+                        correction+=misspelled_word[index+1:]
+                    # test if deleted letter transforms our word to w
+                    if correction == w:
+                        suggestions.append(w)
+                        break
+        return suggestions
     
+    def get_add_corrections(self, misspelled_word):
+        suggestions = []
+        for w in self.words:
+            # only consider words that are one character longer than misspelled word
+            if len(w) == len(misspelled_word)+1:
+                for index in range(0, len(w)):
+                    if w[0:index] == misspelled_word[0:index] and w[index+1:] == misspelled_word[index:]:
+                        suggestions.append(w)
+                        break
+        return suggestions
