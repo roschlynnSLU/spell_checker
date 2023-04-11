1797d4f5feb2d78498ac5317df6ad22add34de0d
roschlynnSLU <127329528+roschlynnSLU@users.noreply.github.com>
2023-04-11 19:36:00 +0000

    Added notes as comments

--- spell_checker.py
+++ spell_checker.py
@@ -1,15 +1,14 @@
-from words import Words
+from words import Words #Import the py file
 
-
-english_words = Words('english.txt')
-sentence = input('Enter a sentence: ')
-word_list = sentence.split(' ')
-for word in word_list:
-    if not english_words.is_word(word.strip()):
-        print('correcting', word)
-        corrections = english_words.get_replace_corrections(word)
-        print('Replace suggestions: ', corrections)
-        corrections = english_words.get_delete_corrections(word)
+english_words = Words('english.txt') #Pass the txt file to the class
+sentence = input('Enter a sentence: ') #Enter the sentence to be checked
+word_list = sentence.split(' ') #Split the sentence into individual words
+for word in word_list: #Loop through every word
+    if not english_words.is_word(word.strip()): #Check if the word is present in the file or not
+        print('correcting', word) #Begin correcting the word
+        corrections = english_words.get_replace_corrections(word) #First get the word suggetions that can be replaced
+        print('Replace suggestions: ', corrections) #Show the replace suggestions to the user
+        corrections = english_words.get_delete_corrections(word) #Then get suggestions that can be shown by deletion
         print('Delete suggestions: ', corrections)
-        corrections = english_words.get_add_corrections(word)
+        corrections = english_words.get_add_corrections(word) #Then get suggestions that can be added by addition of words
         print('Add suggestions: ', corrections)
--- words.py
+++ words.py
@@ -1,35 +1,35 @@
 class Words:
-    def __init__(self, file_name):
-        self.words = []
-        with open(file_name) as word_dictionary:
-            for word in word_dictionary:
-                self.words.append(word.strip('\n'))
+    def __init__(self, file_name): #Constructor takes file name as argument
+        self.words = [] #Define list
+        with open(file_name) as word_dictionary: #Open the file for reading
+            for word in word_dictionary: #Read until file ends
+                self.words.append(word.strip('\n')) #Remove trailing white spaces and append to the list
     
-    def is_word(self, word):
-        return word in self.words
+    def is_word(self, word): #Check whether word is present in the list or not
+        return word in self.words #Returns true or false depending on whether the words are present in the list or not
 
-    def get_size(self):
-        return len(self.words)
+    def get_size(self): #Get the size of the word
+        return len(self.words) #Returns the size of the word
 
-    def get_replace_corrections(self, misspelled_word):
-        suggestions = []
-        for w in self.words:
+    def get_replace_corrections(self, misspelled_word): #Returns the potential list of corrections for the word
+        suggestions = [] #Creates a list to store the suggestions
+        for w in self.words: #Loop through all the words found from the file
             # only consider words that have the same length as misspelled_word
             if len(w) == len(misspelled_word):
                 # try replacing one letter at a time and see if we can transform the misspelled word into word w
-                for index in range(0, len(w)):
-                    correction = misspelled_word[0:index]+w[index]
-                    if index < len(w)-1:
-                        correction+=misspelled_word[index+1:]
+                for index in range(0, len(w)): #Loop through every position of the word
+                    correction = misspelled_word[0:index]+w[index] #Construct a new string called correction by replacing the letter at that position
+                    if index < len(w)-1: #Check if the index is less than the length of the word
+                        correction+=misspelled_word[index+1:] #Correction transformation
                     # test if replaced letter transforms our word to w    
-                    if correction == w:
-                        suggestions.append(w)
+                    if correction == w: #If transformed word matches the word
+                        suggestions.append(w) #Append the transformed word to suggestions list and exit the loop
                         break
-        return suggestions
+        return suggestions #Return the suggested words
         
-    def get_delete_corrections(self, misspelled_word):
-        suggestions = []
-        for w in self.words:
+    def get_delete_corrections(self, misspelled_word): #Check if transformation is possible by deletion
+        suggestions = [] #Initialize suggestions list
+        for w in self.words: #Loop through the words
             # only consider words that are one character shorter than misspelled_word
             if len(w) == len(misspelled_word)-1:
                 # try deleting one character from misspelled word at a time and see if it transforms to word w
@@ -46,14 +46,14 @@ class Words:
                     suggestions.append(w)
         return suggestions
 
-    def can_ransform_with_delete(self, source, dest):
-        transformed = False
-        for index in range(0, len(source)):
-            correction = source[0:index]
-            if index < len(source)-1:
+    def can_ransform_with_delete(self, source, dest): #Check whether source and destionation word match
+        transformed = False #Assume false by default
+        for index in range(0, len(source)): #Loop through every letter of the word
+            correction = source[0:index] #Create a new correction string from 0 to the end of the character string
+            if index < len(source)-1: #Append the string from the omitted letter till the end
                 correction+=source[index+1:]
             # test if deleted letter transforms our word to w
-            if correction == dest:
+            if correction == dest: #Check if the transofrmed letter is equal to the destination then set to true
                 transformed = True
                 break
-        return transformed
+        return transformed #Return the transformed word
